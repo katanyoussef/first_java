a1354b908f64ebc728309e188dd150e4f1dfbaf6
root <root@ip-172-31-93-178.ec2.internal>
2019-02-16 02:58:16 +0000

    soubahnna allah added

--- firstcode.java
+++ firstcode.java
@@ -1,7 +1,7 @@
 public class firstcode{
        public static void main(String[] args){
-                   for(int i=1;i<=10;i++){
-                       System.out.println("hamdolillah...."+i);
+                   for(int i=1;i<=5;i++){
+                       System.out.println("hamdolillah.oubhanna allah.."+i);
                    }
        }
 }
