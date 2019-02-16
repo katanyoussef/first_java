49305c42deda1d5f59d190b9fb65b857938b37ed
root <root@ip-172-31-93-178.ec2.internal>
2019-02-16 02:36:17 +0000

    first commit

--- firstcode.java
+++ firstcode.java
@@ -1,4 +1,4 @@
-public class first_code{
+public class firstcode{
        public static void main(String[] args){
                    for(int i=1;i<=10;i++){
                        System.out.println("hamdolillah...."+i);
